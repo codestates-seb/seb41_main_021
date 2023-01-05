c1bf7b4baa9cad70a8e0b999ff3995f831833a55
문재웅 <111180375+mjwoong@users.noreply.github.com>
2023-01-05 17:41:59 +0900

    style: modify css (#5)
    homepage 로고 첨부, 기본 css 설정

--- frontend/src/pages/TabnidaList.jsx
+++ frontend/src/pages/TabnidaList.jsx
@@ -3,15 +3,17 @@ import styled from 'styled-components';
 export default function TabnidaList() {
   return (
     <>
-      <Container>Tabnida List</Container>
+      <Container></Container>
     </>
   );
 }
 
 const Container = styled.div`
   width: 100%;
-  height: 100px;
-  background-color: pink;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 
   // 태블릿 : 1200px ~ 768px :: 768px 이상 적용되는 css
   @media only screen and (min-width: 768px) {
