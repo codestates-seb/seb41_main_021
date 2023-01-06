9f9537b56fc9cace3ca26ae8521a98a731f255a7
Joo614 <57wnguswn57@gmail.com>
2023-01-07 00:49:04 +0900

    feat: 커밋 안된 부분 추가

---
+++ backend/src/main/java/com/yata/backend/domain/checklist/entity/Checklist.java
@@ -0,0 +1,41 @@
+package com.yata.backend.domain.checklist.entity;
+
+import com.yata.backend.domain.review.entity.ReviewChecklist;
+import com.yata.backend.domain.yata.entity.YataChecklist;
+import lombok.Getter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Checklist {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long checklistId;
+
+    @Column(nullable = false)
+    private String checkContent;
+
+    @Column(nullable = false)
+    private boolean checkPN;
+
+    private enum CheckKind {
+        REVIEW("리뷰"),
+        NEOTA("운전자"),
+        NATA("탑승자");
+
+        @Getter
+        private String status;
+
+        CheckKind(String status) {
+            this.status = status;
+        }
+    }
+
+    @OneToMany(mappedBy = "checklist", cascade = CascadeType.ALL)
+    private List<YataChecklist> yataChecklists = new ArrayList<>();
+
+    @OneToMany(mappedBy = "checklist", cascade = CascadeType.ALL)
+    private List<ReviewChecklist> reviewChecklists = new ArrayList<>();
+}
