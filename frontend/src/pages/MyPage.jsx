732ab27ef91885f02f09ab3c5eaae3733893faca
문재웅 <111180375+mjwoong@users.noreply.github.com>
2023-01-11 10:20:49 +0900

    Style/fe/UI/005 (#64)

--- frontend/src/pages/MyPage.jsx
+++ frontend/src/pages/MyPage.jsx
@@ -6,6 +6,7 @@ import help from '../images/mypage/help.png';
 import carpool from '../images/mypage/carpool.png';
 import like from '../images/mypage/like.png';
 import arrow from '../images/mypage/arrow.png';
+import { NavLink } from 'react-router-dom';
 
 export default function MyPage() {
   return (
@@ -15,7 +16,7 @@ export default function MyPage() {
         <MyPageContainer>
           <ProfileContainer>
             <Profile>
-              <img src={profile} alt="profileImage"></img>
+              <img src={profile} alt="profileImage" width={100} height={100}></img>
               <Info>
                 <div className="text">
                   <b>문재웅</b>
@@ -27,41 +28,37 @@ export default function MyPage() {
             </Profile>
           </ProfileContainer>
           <OilContainer>
-            <GrayBox>
-              <OilLevel>
-                <div className="level">
-                  기름통 레벨<img src={help} alt="QuestionMark" width={10}></img>
-                </div>
-                <img src={oil} alt="oil" width={40}></img>
-                <div className="liter"> 70L </div>
-              </OilLevel>
-              <RightBox>
-                <TripNumber>
-                  <img src={carpool} alt="car" width={35}></img>
-                  <div className="number">30</div>
-                  <div className="text">여정 횟수</div>
-                </TripNumber>
-                <Compliment>
-                  <img src={like} alt="thumbsUp" width={35}></img>
-                  <ComplimentText>
-                    <div className="top">친절해요</div>
-                    <div className="bottom">가장 많이 받은 칭찬</div>
-                  </ComplimentText>
-                </Compliment>
-              </RightBox>
-            </GrayBox>
+            <TripNumber>
+              <img src={carpool} alt="car" width={35} height={35}></img>
+              <div className="title">여정 횟수</div>
+              <div className="bottom">30</div>
+            </TripNumber>
+            <OilLevel>
+              <img src={oil} alt="oil" width={35} height={35}></img>
+              <div className="title">기름통 레벨</div>
+              <div className="bottom"> 70L </div>
+            </OilLevel>
+            <Compliment>
+              <img src={like} alt="thumbsUp" width={35} height={35}></img>
+              <div className="title">많이 받은 칭찬</div>
+              <div className="bottom body">친절해요</div>
+            </Compliment>
           </OilContainer>
           <ListContainer>
+            <NavLink className={({ isActive }) => (isActive ? 'active' : 'not')} to="/journeylist">
+              <JourneyRecord>
+                <div className="title">여정 내역</div>
+                <img src={arrow} alt="arrow" width={30}></img>
+              </JourneyRecord>
+            </NavLink>
+            <NavLink className={({ isActive }) => (isActive ? 'active' : 'not')} to="/ratinglist">
+              <JourneyRecord>
+                <div className="title">받은 매너 평가</div>
+                <img src={arrow} alt="arrow" width={30}></img>
+              </JourneyRecord>
+            </NavLink>
             <JourneyRecord>
-              <div className="text">여정 내역</div>
-              <img src={arrow} alt="arrow" width={30}></img>
-            </JourneyRecord>
-            <JourneyRecord>
-              <div className="text">여정 내역</div>
-              <img src={arrow} alt="arrow" width={30}></img>
-            </JourneyRecord>
-            <JourneyRecord>
-              <div className="text">여정 내역</div>
+              <div className="title">-</div>
               <img src={arrow} alt="arrow" width={30}></img>
             </JourneyRecord>
           </ListContainer>
@@ -92,22 +89,23 @@ const MyPageContainer = styled.div`
   height: 100vh;
   display: flex;
   flex-direction: column;
+  align-items: center;
 `;
 const ProfileContainer = styled.div`
   width: 100%;
-  height: 20%;
-  background-color: #617db9;
-  display: flex;
-  align-items: center;
+  height: 25%;
+  background-color: ${props => props.theme.colors.main_blue};
+  box-shadow: 0px 4px 4px -4px #3f5179;
 `;
 const Profile = styled.div`
   width: 100%;
   height: 100%;
   padding: 0 7rem 0 7rem;
+  position: relative;
+  bottom: 3rem;
   display: flex;
   align-items: center;
   justify-content: center;
-  box-shadow: 0px 4px 4px -4px #3f5179;
 `;
 const Info = styled.div`
   width: 60%;
@@ -119,105 +117,78 @@ const Info = styled.div`
   color: #fff;
   div {
     margin-top: 0.3rem;
+    font-size: 1.2rem;
   }
 `;
 const OilContainer = styled.div`
-  width: 100%;
-  height: 30%;
-  box-shadow: 0px 2px 2px -2px #a4a3a4;
-  padding: 5% 10% 5% 10%;
+  width: 90%;
+  height: 12rem;
+  background-color: #fff;
+  border-radius: 1rem;
+  box-shadow: 0 0px 10px #73b2d9, 0 0px 10px #73b2d9;
+  position: relative;
+  bottom: 5rem;
   display: flex;
+  flex-direction: row;
   align-items: center;
-  justify-content: center;
-  justify-content: center;
-`;
-const GrayBox = styled.div`
-  width: 100%;
-  height: 90%;
-  background-color: #f0f1f0;
-  border-radius: 2rem;
-  padding: 3rem;
-  display: flex;
-`;
-const RightBox = styled.div`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-`;
-const OilLevel = styled.div`
-  width: 55%;
-  height: 100%;
-  border-right: 1px solid black;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  padding: 1rem 2rem 1rem 0;
-  .level {
-    font-size: 0.8rem;
-    color: gray;
-    margin-bottom: 1rem;
-  }
-  .liter {
-    font-size: 3rem;
+  justify-content: space-around;
+  .title {
     font-weight: 600;
-    margin-top: 1rem;
-  }
-`;
-const TripNumber = styled.div`
-  width: 100%;
-  height: 50%;
-  padding-left: 1rem;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  .number {
-    margin: 0 0.5rem 0 0.5rem;
-    font-size: 2.5rem;
-    font-weight: 600;
-  }
-  .text {
-    font-size: 0.8rem;
-    color: gray;
-  }
-`;
-const Compliment = styled.div`
-  width: 100%;
-  height: 50%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-const ComplimentText = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  .top {
-    font-size: 2rem;
-    font-weight: 600;
-    margin-bottom: 0.5rem;
+    margin: 0.5rem 0 0.5rem 0;
+    color: ${props => props.theme.colors.main_blue};
   }
   .bottom {
-    font-size: 0.8rem;
-    color: gray;
+    color: ${props => props.theme.colors.gray};
+    font-size: 1.8rem;
+    font-weight: 600;
+  }
+`;
+const OilLevel = styled.div`
+  width: 7rem;
+  height: 7rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+const TripNumber = styled.div`
+  width: 7rem;
+  height: 7rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+const Compliment = styled.div`
+  width: 7rem;
+  height: 7rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  bottom: 0.2rem;
+  .body {
+    color: ${props => props.theme.colors.gray};
+    font-size: 1.4rem;
+    font-weight: 600;
   }
 `;
 const ListContainer = styled.div`
   width: 100%;
-  height: 30%;
+  height: auto;
+  box-shadow: 0px -2px 2px -2px ${props => props.theme.colors.light_gray};
 `;
 const JourneyRecord = styled.div`
   width: 100%;
-  height: 33.3%;
-  box-shadow: 0px 2px 2px -2px #a4a3a4;
+  height: 6rem;
+  box-shadow: 0px 2px 2px -2px ${props => props.theme.colors.light_gray};
   padding: 0 3rem 0 3rem;
   display: flex;
   align-items: center;
   justify-content: space-between;
-  .text {
-    font-size: 1.5rem;
+  .title {
+    font-size: 1.3rem;
     font-weight: 600;
   }
 `;
