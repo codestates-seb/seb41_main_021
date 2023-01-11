d0bb281e6ac05691ae61ba87e68438ca61a13c55
yoo hyeong chan <84506708+gudcks0305@users.noreply.github.com>
2023-01-11 15:42:11 +0900

    feature : redis logout JWT blackList

--- backend/src/main/java/com/yata/backend/global/config/redis/RedisRepositoryConfig.java
+++ backend/src/main/java/com/yata/backend/global/config/redis/RedisRepositoryConfig.java
@@ -1,31 +1,50 @@
 package com.yata.backend.global.config.redis;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
+import java.util.Arrays;
+
 @Configuration
 @EnableRedisRepositories
 public class RedisRepositoryConfig {
-   @Value("${spring.redis.host}")
-   private String redisHost;
+    @Value("${spring.redis.host}")
+    private String redisHost;
 
-   @Value("${spring.redis.port}")
-   private int redisPort;
+    @Value("${spring.redis.port}")
+    private int redisPort;
 
-   @Bean
-   public RedisConnectionFactory redisConnectionFactory() {
-      return new LettuceConnectionFactory(redisHost, redisPort);
-   }
+    @Value(value = "${spring.redis.password}")
+    private String redisPassword;
+    @Autowired
+    private Environment environment;
 
-   @Bean
-   public RedisTemplate<?, ?> redisTemplate() {
-      RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
-      redisTemplate.setConnectionFactory(redisConnectionFactory());
-      return redisTemplate;
-   }
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(redisHost);
+        redisStandaloneConfiguration.setPort(redisPort);
+        // redis password 설정
+        Arrays.stream(environment.getActiveProfiles()).forEach(profile -> {
+            if (profile.equals("prod")) {
+                redisStandaloneConfiguration.setPassword(redisPassword);
+            }
+        });
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
+    }
+
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate() {
+        RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        return redisTemplate;
+    }
 }
