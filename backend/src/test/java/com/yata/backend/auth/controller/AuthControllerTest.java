e7d68fce78b32a3730f0805774b603d3cd117208
yoo hyeong chan <84506708+gudcks0305@users.noreply.github.com>
2023-01-11 17:13:22 +0900

    fix : testing settings change

--- backend/src/test/java/com/yata/backend/auth/controller/AuthControllerTest.java
+++ backend/src/test/java/com/yata/backend/auth/controller/AuthControllerTest.java
@@ -14,11 +14,13 @@ import com.yata.backend.global.utils.RedisUtils;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
