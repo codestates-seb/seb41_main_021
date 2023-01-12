3957b6c40954b433d49dafbce95c7d02596ea5f0
yoo hyeong chan <84506708+gudcks0305@users.noreply.github.com>
2023-01-12 17:40:47 +0900

    test : member verification

--- backend/src/main/java/com/yata/backend/domain/member/controller/SignUpVerifyController.java
+++ backend/src/main/java/com/yata/backend/domain/member/controller/SignUpVerifyController.java
@@ -18,15 +18,18 @@ public class SignUpVerifyController {
         this.memberService = memberService;
     }
     @GetMapping("/email/{email}")
+    // 이메일 중복검사
     public ResponseEntity duplicateMember(@PathVariable  String email) {
         boolean dup = memberService.duplicateEmail(email);
         return ResponseEntity.ok(new SingleResponse<>(dup));
     }
+    // 이메일 인증 메일 보내는 컨트롤러
     @PostMapping("/email/{email}")
-    public ResponseEntity verifyMember(@PathVariable  String email) {
+    public ResponseEntity verifyEmailAuth(@PathVariable  String email) {
         signUpVerifyService.sendAuthMail(email);
         return ResponseEntity.noContent().build();
     }
+    // 이메일 인증 코드 검증
     @PostMapping("/auth-code")
     public ResponseEntity verifyAuthCode(@RequestBody EmailAuthDto emailAuthDto) {
         boolean verified = signUpVerifyService.verifyAuthCode(emailAuthDto);
