1503ef1a9a991cbdfc5166de061077a81639a7f9
문재웅 <answodnd112@gmail.com>
2023-01-12 11:56:33 +0900

    style: modify css

--- frontend/src/pages/DriverAuth.jsx
+++ frontend/src/pages/DriverAuth.jsx
@@ -1,11 +1,19 @@
 import styled from 'styled-components';
 import Header from '../components/Header';
+import Input from '../components/common/Input';
+import Button from '../components/common/Button';
 
 export default function DriverAuth() {
   return (
     <>
       <Container>
         <Header title="운전자 인증하기" />
+        <Contents>
+          <Input label="이름"></Input>
+          <Input label="운전면허 번호"></Input>
+          <Input label="발급일자"></Input>
+          <AuthBtn>인증하기</AuthBtn>
+        </Contents>
       </Container>
     </>
   );
@@ -14,4 +22,23 @@ export default function DriverAuth() {
 const Container = styled.div`
   width: 100%;
   height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+const Contents = styled.div`
+  width: 80%;
+  max-width: 600px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 3rem;
+  @media screen and (min-width: 800px) {
+    margin-top: 10rem;
+  }
+`;
+const AuthBtn = styled(Button)`
+  width: 100%;
+  height: 3rem;
+  margin: 4rem;
 `;
