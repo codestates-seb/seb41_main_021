38db9196d1e0de335ea3e4dfc271424eeedae1e6
Hyunji0911 <hyunji.lee0911@gmail.com>
2023-01-17 14:09:49 +0900

    style: add placeholder

--- frontend/src/pages/ExchangePage.jsx
+++ frontend/src/pages/ExchangePage.jsx
@@ -9,9 +9,9 @@ export default function ExchagePage() {
       <Container>
         <Header title="포인트 환전하기" />
         <Contents>
-          <Input label="은행"></Input>
-          <Input label="계좌 번호"></Input>
-          <Input label="환전 금액"></Input>
+          <Input label="거래 은행" placeholder="거래 은행 입력"></Input>
+          <Input label="계좌 번호" placeholder='"-" 없이 입력'></Input>
+          <Input label="환전 금액" placeholder="환전 금액 입력"></Input>
           <ExchangeBtn>환전하기</ExchangeBtn>
         </Contents>
       </Container>
