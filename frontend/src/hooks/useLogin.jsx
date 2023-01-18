4f6048e79e2cb9c320d86f6641655de893861a4d
Lee Hyun Ji <107021273+Hyunji0911@users.noreply.github.com>
2023-01-18 18:31:27 +0900

    feature: redux toolkit & axios first setting (#169)

---
+++ frontend/src/hooks/useLogin.jsx
@@ -0,0 +1,59 @@
+import axios from 'axios';
+
+const getErrorType = status => {
+  if (status === 403) {
+    return 'incorrectPassword';
+  }
+  if (status === 504) {
+    return 'invalidEmail';
+  }
+  return 'regular';
+};
+
+const useLogin = async (url, data) => {
+  try {
+    const response = await axios.post('https://server.yata.kro.kr/api/v1/auth/login', data);
+    if (response.headers.authorization) {
+      //   setToken({
+      //     ACCESS: `Bearer ${response.headers.authorization}`,
+      //     REFRESH: response.headers.refreshtoken,
+      //   });
+    }
+    await localStorage.setItem('ACCESS', response.headers.authorization);
+    await localStorage.setItem('REFRESH', response.headers.refreshtoken);
+
+    // setUserMbti(response.data.mbti);
+    // setIsLogIn(true);
+    // invalidate();
+  } catch (error) {
+    const validationType = getErrorType(error.response.data.status);
+
+    if (validationType !== 'regular') return setErrorException(validationType);
+  }
+  return null;
+};
+
+const useLogout = async (url, data) => {
+  try {
+    const response = await axios.post('https://server.yata.kro.kr/api/v1/auth/logout', data);
+    if (response.headers.authorization) {
+      //   setToken({
+      //     ACCESS: `Bearer ${response.headers.authorization}`,
+      //     REFRESH: response.headers.refreshtoken,
+      //   });
+    }
+    await localStorage.setItem('ACCESS', response.headers.authorization);
+    await localStorage.setItem('REFRESH', response.headers.refreshtoken);
+
+    // setUserMbti(response.data.mbti);
+    // setIsLogIn(true);
+    // invalidate();
+  } catch (error) {
+    const validationType = getErrorType(error.response.data.status);
+
+    if (validationType !== 'regular') return setErrorException(validationType);
+  }
+  return null;
+};
+
+export { useLogin, useLogout };
---
+++ frontend/src/hooks/usePostData.jsx
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const header = {
+  headers: {
+    'Content-Type': 'application/json;charset=UTF-8',
+    Accept: 'application/json',
+  },
+};
+
+const usePostData = async (url, data) => {
+  try {
+    if (data) {
+      const response = await axios.get(url, data, header);
+      return response;
+    }
+    const response = await axios.get(url, header);
+    return response;
+  } catch (error) {
+    return console.log(error);
+  }
+};
+
+export default usePostData;
