a780c8d4eaf8f4997a937f70332f5bc7fef064eb
yoo hyeong chan <84506708+gudcks0305@users.noreply.github.com>
2023-01-22 18:07:37 +0900

    feature : 야타 request 에 대한 aop, async를 활용한 알림 기능

---
+++ backend/src/main/java/com/yata/backend/domain/notify/aop/NotifyAspect.java
@@ -0,0 +1,48 @@
+package com.yata.backend.domain.notify.aop;
+
+import com.yata.backend.domain.notify.dto.NotifyMessage;
+import com.yata.backend.domain.notify.entity.Notify;
+import com.yata.backend.domain.notify.service.NotifyService;
+import com.yata.backend.domain.yata.entity.YataRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Slf4j
+@Component
+@EnableAsync
+public class NotifyAspect {
+    // yataRequestService  createRequest method pointcut
+    private final NotifyService notifyService;
+
+    public NotifyAspect(NotifyService notifyService) {
+        this.notifyService = notifyService;
+    }
+
+    @Pointcut("execution(* com.yata.backend.domain.yata.service.YataRequestService.createRequest(..))")
+    public void requestNotify() {
+    }
+    @Pointcut("execution(* com.yata.backend.domain.yata.service.YataRequestService.createInvitation(..))")
+    public void inviteNotify() {
+    }
+    // 포인트 컷 합치기
+    public void notifying(){}
+
+    @Async
+    @AfterReturning(pointcut = "requestNotify() || inviteNotify()", returning = "result")
+    public void checkValue(JoinPoint joinPoint, Object result) throws Throwable {
+        notifyService.send(
+                ((YataRequest) result).getYata().getMember(),
+                Notify.NotificationType.YATA,
+                NotifyMessage.YATA_NEW_REQUEST.getMessage(),
+                "/api/v1/yata/" + ((YataRequest) result).getYata().getYataId()
+        );
+        log.info("result = {}", result);
+    }
+}
--- backend/src/main/java/com/yata/backend/domain/notify/controller/NotifyController.java
+++ backend/src/main/java/com/yata/backend/domain/notify/controller/NotifyController.java
@@ -1,12 +1,12 @@
 package com.yata.backend.domain.notify.controller;
 
 import com.yata.backend.domain.notify.service.NotifyService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 @RestController
+@RequestMapping("/api/v1/notify")
 public class NotifyController {
    private final NotifyService notifyService;
 
@@ -15,11 +15,9 @@ public class NotifyController {
    }
 
 
-
-
-   @GetMapping(value = "/subscribe/{id}", produces = "text/event-stream")
-   public SseEmitter subscribe(@PathVariable String id,
+   @GetMapping(value = "/subscribe", produces = "text/event-stream")
+   public SseEmitter subscribe(@AuthenticationPrincipal User principal,
                                @RequestHeader(value = "Last-Event-ID", required = false, defaultValue = "") String lastEventId) {
-      return notifyService.subscribe(id, lastEventId);
+      return notifyService.subscribe(principal.getUsername(), lastEventId);
    }
 }
