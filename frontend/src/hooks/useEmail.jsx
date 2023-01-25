fe8b0dfb0197b721719104a39b978e51f4250fa7
문재웅 <answodnd112@gmail.com>
2023-01-25 16:31:11 +0900

    feature: email auth

---
+++ frontend/src/hooks/useEmail.jsx
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+const header = {
+  headers: {
+    'Content-Type': 'application/json;charset=UTF-8',
+  },
+};
+
+const useEmailAuth = async email => {
+  try {
+    const response = await axios.post(`https://server.yata.kro.kr/api/v1/validation/email/${email}`, header);
+    return console.log(response);
+  } catch (error) {
+    return console.log(error);
+  }
+};
+
+const useEmailAuthConfirm = async body => {
+  try {
+    const response = await axios.post(`https://server.yata.kro.kr/api/v1/validation/auth-code`, body, header);
+    return response;
+  } catch (error) {
+    return console.log(error);
+  }
+};
+
+export { useEmailAuth, useEmailAuthConfirm };
