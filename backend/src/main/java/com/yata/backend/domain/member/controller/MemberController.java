bd0ace6d3284a44aa8fbbcf1dbde0400a542c12c
yoo hyeong chan <84506708+gudcks0305@users.noreply.github.com>
2023-01-27 14:40:00 +0900

    feature : 운전자 인증 삭제 문제 해결 , 타 유저정보 조회 api

--- backend/src/main/java/com/yata/backend/domain/member/controller/MemberController.java
+++ backend/src/main/java/com/yata/backend/domain/member/controller/MemberController.java
@@ -19,33 +19,43 @@ import java.net.URI;
 @RequestMapping("/api/v1/members")
 @Validated
 public class MemberController {
-   private final MemberService memberService;
-   private final MemberMapper memberMapper;
-   private static final String BASE_URL = "/api/v1/members";
-   private final YataMemberService yataMemberService;
+    private final MemberService memberService;
+    private final MemberMapper memberMapper;
+    private static final String BASE_URL = "/api/v1/members";
+    private final YataMemberService yataMemberService;
 
-   public MemberController(MemberService memberService, MemberMapper memberMapper, YataMemberService yataMemberService) {
-      this.memberService = memberService;
-      this.memberMapper = memberMapper;
-      this.yataMemberService = yataMemberService;
-   }
-   @PostMapping("/signup")
-   public ResponseEntity signup(@Valid @RequestBody MemberDto.Post memberPostDto) {
-      Member member = memberService.createMember(memberMapper.memberPostDtoToMember(memberPostDto));
-      return ResponseEntity.created(URI.create(BASE_URL)).build();
-   }
-   @GetMapping
-   public ResponseEntity getMember(@AuthenticationPrincipal User principal) {
-      MemberDto.Response member = memberService.findMemberDto(principal.getUsername());
-      member.setYataCount(yataMemberService.myYataCount(principal.getUsername()));
-      return ResponseEntity.ok(new SingleResponse<>(member));
-   }
-   @PatchMapping
-   public ResponseEntity updateMember(@AuthenticationPrincipal User principal, @Valid @RequestBody MemberDto.Patch memberPatchDto) {
-      Member member = memberService.updateMember(principal.getUsername(), memberMapper.memberPatchDtoToMember(memberPatchDto));
-      return ResponseEntity.ok(new SingleResponse<>(memberMapper.memberToResponseMemberDto(member)));
-   }
+    public MemberController(MemberService memberService, MemberMapper memberMapper, YataMemberService yataMemberService) {
+        this.memberService = memberService;
+        this.memberMapper = memberMapper;
+        this.yataMemberService = yataMemberService;
+    }
 
+    @PostMapping("/signup")
+    public ResponseEntity signup(@Valid @RequestBody MemberDto.Post memberPostDto) {
+        Member member = memberService.createMember(memberMapper.memberPostDtoToMember(memberPostDto));
+        return ResponseEntity.created(URI.create(BASE_URL)).build();
+    }
+
+    @GetMapping
+    public ResponseEntity getMember(@AuthenticationPrincipal User principal) {
+        MemberDto.Response member = memberService.findMemberDto(principal.getUsername());
+        member.setYataCount(yataMemberService.yataCount(principal.getUsername()));
+        return ResponseEntity.ok(new SingleResponse<>(member));
+    }
+
+    @PatchMapping
+    public ResponseEntity updateMember(@AuthenticationPrincipal User principal, @Valid @RequestBody MemberDto.Patch memberPatchDto) {
+        Member member = memberService.updateMember(principal.getUsername(), memberMapper.memberPatchDtoToMember(memberPatchDto));
+        return ResponseEntity.ok(new SingleResponse<>(memberMapper.memberToResponseMemberDto(member)));
+    }
+
+    // 타 유저 조회
+    @GetMapping("/{email}")
+    public ResponseEntity getMember(@PathVariable String email) {
+        MemberDto.Response member = memberService.findMemberDto(email);
+        member.setYataCount(yataMemberService.yataCount(email));
+        return ResponseEntity.ok(new SingleResponse<>(member));
+    }
 
 
 }
--- backend/src/test/java/com/yata/backend/domain/member/controller/MemberControllerTest.java
+++ backend/src/test/java/com/yata/backend/domain/member/controller/MemberControllerTest.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.restdocs.payload.ResponseFieldsSnippet;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.ResultActions;
@@ -22,7 +23,6 @@ import org.springframework.test.web.servlet.ResultActions;
 import static com.yata.backend.domain.member.factory.MemberFactory.createMemberPostDto;
 import static com.yata.backend.utils.ApiDocumentUtils.getRequestPreProcessor;
 import static com.yata.backend.utils.ApiDocumentUtils.getResponsePreProcessor;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.restdocs.headers.HeaderDocumentation.*;
@@ -30,6 +30,8 @@ import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.docu
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -107,22 +109,51 @@ class MemberControllerTest extends AbstractControllerTest {
                         requestHeaders(
                                 headerWithName("Authorization").description("JWT 토큰")
                         ),
-                        responseFields(
-                                fieldWithPath("data").type(JsonFieldType.OBJECT).description("회원 정보"),
-                                fieldWithPath("data.email").type(JsonFieldType.STRING).description("이메일"),
-                                fieldWithPath("data.name").type(JsonFieldType.STRING).description("이름 실명"),
-                                fieldWithPath("data.nickname").type(JsonFieldType.STRING).description("닉네임"),
-                                fieldWithPath("data.genders").type(JsonFieldType.STRING).description("성별"),
-                                fieldWithPath("data.imgUrl").type(JsonFieldType.STRING).description("프로필 이미지"),
-                                fieldWithPath("data.carImgUrl").type(JsonFieldType.STRING).description("자동차 이미지"),
-                                fieldWithPath("data.providerType").type(JsonFieldType.STRING).description("로그인 타입"),
-                                fieldWithPath("data.roles").type(JsonFieldType.ARRAY).description("로그인 권한 정보 리스트"),
-                                fieldWithPath("data.roles[]").type(JsonFieldType.ARRAY).description("로그인 권한 정보 DRIVER,PASSANGER ,ADMIN"),
-                                fieldWithPath("data.point").type(JsonFieldType.NUMBER).description("포인트 잔액"),
-                                fieldWithPath("data.memberStatus").type(JsonFieldType.STRING).description("맴버 상태"),
-                                fieldWithPath("data.fuelTank").type(JsonFieldType.NUMBER).description("연료탱크 점수"),
-                                fieldWithPath("data.yataCount").type(JsonFieldType.NUMBER).description("여정 횟수 20분 캐싱")
-                        )
+                        getMemberResponseFieldsSnippet()
                 ));
+    }
+    @Test
+    @DisplayName("타 회원 정보 조회")
+    @WithMockUser
+    void getMemberInfo() throws Exception {
+        // given
+        Member member = MemberFactory.createMember(new BCryptPasswordEncoder());
+        given(memberService.findMemberDto(any())).willReturn(MemberFactory.createMemberResponseDto(member));
+        // when
+        ResultActions resultActions = mockMvc.perform(get(BASE_URL + "/{email}", member.getEmail())
+                        .contentType("application/json")
+                        .headers(GeneratedToken.getMockHeaderToken())
+                        .with(csrf()))
+                .andExpect(status().isOk());
+        // then
+        resultActions.andDo(print());
+        resultActions.andDo(
+                document("member-get-others",
+                        getRequestPreProcessor(),
+                        getResponsePreProcessor(),
+                        requestParameters(
+                                parameterWithName("email").description("조회할 회원의 이메일"),
+                                parameterWithName("_csrf").description("무시!")
+                        ),
+                        getMemberResponseFieldsSnippet()
+                ));
+    }
+    private static ResponseFieldsSnippet getMemberResponseFieldsSnippet() {
+        return responseFields(
+                fieldWithPath("data").type(JsonFieldType.OBJECT).description("회원 정보"),
+                fieldWithPath("data.email").type(JsonFieldType.STRING).description("이메일"),
+                fieldWithPath("data.name").type(JsonFieldType.STRING).description("이름 실명"),
+                fieldWithPath("data.nickname").type(JsonFieldType.STRING).description("닉네임"),
+                fieldWithPath("data.genders").type(JsonFieldType.STRING).description("성별"),
+                fieldWithPath("data.imgUrl").type(JsonFieldType.STRING).description("프로필 이미지"),
+                fieldWithPath("data.carImgUrl").type(JsonFieldType.STRING).description("자동차 이미지"),
+                fieldWithPath("data.providerType").type(JsonFieldType.STRING).description("로그인 타입"),
+                fieldWithPath("data.roles").type(JsonFieldType.ARRAY).description("로그인 권한 정보 리스트"),
+                fieldWithPath("data.roles[]").type(JsonFieldType.ARRAY).description("로그인 권한 정보 DRIVER,PASSANGER ,ADMIN"),
+                fieldWithPath("data.point").type(JsonFieldType.NUMBER).description("포인트 잔액"),
+                fieldWithPath("data.memberStatus").type(JsonFieldType.STRING).description("맴버 상태"),
+                fieldWithPath("data.fuelTank").type(JsonFieldType.NUMBER).description("연료탱크 점수"),
+                fieldWithPath("data.yataCount").type(JsonFieldType.NUMBER).description("여정 횟수 20분 캐싱")
+        );
     }
 }
