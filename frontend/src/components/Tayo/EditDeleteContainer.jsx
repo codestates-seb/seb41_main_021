43f034f07e328c430294fa53ad2b1650e921dcfc
문재웅 <answodnd112@gmail.com>
2023-01-28 22:32:14 +0900

    style: modify css

--- frontend/src/components/Tayo/EditDeleteContainer.jsx
+++ frontend/src/components/Tayo/EditDeleteContainer.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router';
 import useDeleteData from '../../hooks/useDeleteData';
 
 export default function EditDeleteContainer(props) {
-  const { state, yataId } = props;
+  const { state, yataId, data, ableTag, disableTag } = props;
   const navigate = useNavigate();
 
   const deleteHandler = () => {
@@ -14,16 +14,29 @@ export default function EditDeleteContainer(props) {
   };
 
   return (
-    <CRUDContainer>
-      <FiEdit onClick={() => navigate(`/${state}-edit/${yataId}`)} title="수정하기" />
-      <FiTrash onClick={deleteHandler} title="삭제하기" />
-    </CRUDContainer>
+    <Container>
+      <TagContainer>
+        <Tag state={data.postStatus}>{data.postStatus === 'POST_OPEN' ? ableTag : disableTag}</Tag>
+      </TagContainer>
+      <CRUDContainer>
+        <FiEdit onClick={() => navigate(`/${state}-edit/${yataId}`)} title="수정하기" />
+        <FiTrash onClick={deleteHandler} title="삭제하기" />
+      </CRUDContainer>
+    </Container>
   );
 }
 
-const CRUDContainer = styled.div`
-  width: 95%;
+const Container = styled.div`
+  display: flex;
+  align-items: center;
   margin-top: 1rem;
+  width: 90%;
+`;
+
+const TagContainer = styled.div``;
+
+const CRUDContainer = styled.div`
+  width: 100%;
   display: flex;
   align-items: center;
   justify-content: flex-end;
@@ -33,4 +46,19 @@ const CRUDContainer = styled.div`
     margin: 0.5rem 0.8rem;
     cursor: pointer;
   }
+`;
+
+const Tag = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-left: 0.5rem;
+  padding: 0.5rem;
+  width: 5rem;
+  height: 1.8rem;
+  color: white;
+  border-radius: 0.2rem;
+
+  background-color: ${props =>
+    props.state === 'POST_OPEN' ? props.theme.colors.main_blue : props.theme.colors.light_gray};
 `;
