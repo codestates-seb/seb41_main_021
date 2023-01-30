36b8175dcdd4463ed31a021c87075453577bc2ed
yoo hyeong chan <84506708+gudcks0305@users.noreply.github.com>
2023-01-30 11:23:26 +0900

    chore : 누락... 기능... 추가...

--- backend/src/main/java/com/yata/backend/domain/yata/service/YataRequestServiceImpl.java
+++ backend/src/main/java/com/yata/backend/domain/yata/service/YataRequestServiceImpl.java
@@ -85,7 +85,7 @@ public class YataRequestServiceImpl implements YataRequestService {
         Member yataOwner = memberService.findMember(userName); // 해당 멤버가 있는지 확인하고
         Member invitedMember = memberService.findMember(invitationRequestDto.getInviteEmail()); // 초대할 멤버가 있는지 확인하고
         // TODO verify 조작
-        verifyInvitation(userName, invitationRequestDto.getYataId()); // 신청 목록에 있는 애인지 확인하고 (초대한 애가 아닌지 확인하고)
+        verifyInvitation(invitationRequestDto.getInviteEmail(), invitationRequestDto.getYataId()); // 신청 목록에 있는 애인지 확인하고 (초대한 애가 아닌지 확인하고)
         // 내가 초대 할려는 게시물이 있는지 확인
         Yata yata = yataService.findYata(invitationRequestDto.getYataId());
         Yata invitedYata = yataService.findYata(invitationRequestDto.getInvitedYataId());
