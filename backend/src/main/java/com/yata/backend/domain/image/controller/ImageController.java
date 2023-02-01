4d54ed5db63bf7805ef9b910ff243ea923ef5093
yoo hyeong chan <84506708+gudcks0305@users.noreply.github.com>
2023-02-01 13:28:19 +0900

    fix : multipartfile boundary exception test

--- backend/src/main/java/com/yata/backend/domain/image/controller/ImageController.java
+++ backend/src/main/java/com/yata/backend/domain/image/controller/ImageController.java
@@ -5,10 +5,7 @@ import com.yata.backend.global.response.SingleResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
