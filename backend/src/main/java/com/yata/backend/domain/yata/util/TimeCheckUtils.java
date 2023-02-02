f1c124949d257dbeae5abddfd388eb7eb66dad52
yoo hyeong chan <84506708+gudcks0305@users.noreply.github.com>
2023-02-02 23:05:12 +0900

    fix : 같은 날 출발이 아닌 글에는 초대 불가능

--- backend/src/main/java/com/yata/backend/domain/yata/util/TimeCheckUtils.java
+++ backend/src/main/java/com/yata/backend/domain/yata/util/TimeCheckUtils.java
@@ -4,11 +4,20 @@ import com.yata.backend.global.exception.CustomLogicException;
 import com.yata.backend.global.exception.ExceptionCode;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
+
 @Component
 public class TimeCheckUtils {
 
     public static void verifyTime(long departureTime, long compareTime) {
         if (departureTime <= compareTime) {
+            throw new CustomLogicException(ExceptionCode.INVALID_TIME);
+        }
+    }
+    public static void isEqualsYearsMonthDay(Date departureTime, Date compareTime) {
+        if (departureTime.getYear() != compareTime.getYear() ||
+                departureTime.getMonth() != compareTime.getMonth() ||
+                departureTime.getDay() != compareTime.getDay()) {
             throw new CustomLogicException(ExceptionCode.INVALID_TIME);
         }
     }
